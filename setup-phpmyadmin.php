748b46b176716ca82050275bdedb8ecc1d4988ba
Max Base <MaxBaseCode@gmail.com>
2025-06-05 23:47:40 +0330

    crack limits

--- setup-phpmyadmin.php
+++ setup-phpmyadmin.php
@@ -1,4 +1,12 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+set_time_limit(0);
+ini_set('memory_limit', '10240000000M');
+ini_set('log_errors', '1');
+ini_set('error_log', __DIR__ . '/phpmyadmin_setup_errors.log');
+
 // --- Helper Functions ---
 function isCli() {
     return (php_sapi_name() === 'cli' || defined('STDIN'));
